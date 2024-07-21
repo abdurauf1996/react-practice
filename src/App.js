720eac7b84283b48f9ee0ae2c234d2f966bbc4f7
abdurauf1996 <vakhobovabdurauf@gmail.com>
2024-07-21 18:29:22 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Title from "./components/Title";
+import Modal from "./components/Modal";
+import EventList from "./components/EventList";
+import NewEventForm from "./components/NewEventForm";
 function App() {
+  const [showModal, setShowmodal] = useState(false);
+  const [showConent, setShowContent] = useState(true);
+  const [events, setEvents] = useState([
+    { title: "abdurauf birthday party", id: 1 },
+    { title: "sevara live strem", id: 2 },
+    { title: "match:  manchester united vs bvarselona", id: 3 },
+  ]);
+
+  // delete items
+  const handleDelete = (id) => {
+    setEvents((prev) => {
+      return prev.filter((event) => {
+        return event.id !== id;
+      });
+    });
+  };
+  //toogle modal function
+  const closeModal = () => {
+    setShowmodal(false);
+  };
+
+  let subtitle = "All events well be here:)";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title
+        title="Abdurauf's Kingdom👑 Events "
+        subtitle={"All events well be here:)"}
+      />
+
+      {showConent && (
+        <button onClick={() => setShowContent(false)}>Hide Conent</button>
+      )}
+      {!showConent && (
+        <button onClick={() => setShowContent(true)}>Show Conent</button>
+      )}
+      {showConent && <EventList events={events} handleDelete={handleDelete} />}
+
+      {showModal && (
+        <Modal closeModal={closeModal} isModeModal={false}>
+          <NewEventForm />
+        </Modal>
+      )}
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setShowmodal(true);
+        }}
+      >
+        New Event
+      </button>
     </div>
   );
 }
---
+++ src/components/NewEventForm.js
@@ -0,0 +1,29 @@
+//style
+import "./NewEventForm.css";
+import { useState } from "react";
+
+function NewEventForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  return (
+    <form className="new-event-form">
+      <label>
+        <span>Event Title</span>
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+      </label>
+      <label>
+        <span>Event Date:</span>
+        <input type="date" onChange={(e) => setDate(e.target.value)} />
+      </label>
+      <button type="button">reset inputs</button>
+      <br />
+      <br />
+      <button>Submit</button>
+      <hr />
+      <p>Title:{title}</p>
+      <p>Date:{date}</p>
+    </form>
+  );
+}
+
+export default NewEventForm;
